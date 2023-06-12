59bd7fd773f0e09a8ffcc0c8bff5c7810d501e70
Sonali <sonali.dandoriya@catalystone.com>
2023-06-12 22:10:54 +0530

    Outputting data dynamically in the component ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const date = new Date(2023, 6, 12);
+  const title = 'Car Insurance';
+  const amount = 294.67;
+
   return (
-    <div className="expense-item">
-      <div>June 12th 2023</div>
-      <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+    <div className='expense-item'>
+      <div>{date.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${amount}</div>
       </div>
     </div>
   );
