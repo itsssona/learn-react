b4158dc435c25780d0560082728743758569d758
Sonali <sonali.dandoriya@catalystone.com>
2023-06-12 22:04:58 +0530

    Writing more complex JSX code by defining more content inside ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,12 @@
 function ExpenseItem() {
-    return <h2>This is an ExpenseItem !!</h2>
+    return (
+        <div>
+            <h2>This is an ExpenseItem !!</h2>
+            <h1> Item: Car</h1>
+            <div>Price: 78,00,000/-</div>
+            <div>Date: 12-06-2023</div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
