ff2b09ebc989419e7fbdac122afe8cb2fef39dd7
Sonali <136353154+itsssona@users.noreply.github.com>
2023-06-13 22:07:25 +0530

    Extracted out Date setup from ExpenseItem component, and created a new component ExpenseDate

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,11 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('en-US', { month: 'long' });
-  const day = props.date.getDate();
-  const year = props.date.getFullYear();
 
   return (
     <div className='expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+        <ExpenseDate date={props.date}></ExpenseDate>
       <div className='expense-item__description'>
         <h2>{props.name}</h2>
         <div className='expense-item__price'>${props.price}</div>
