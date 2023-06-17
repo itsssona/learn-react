75e5ed491b5467c0a13cfc549a00e3d69cd98ca9
Sonali <136353154+itsssona@users.noreply.github.com>
2023-06-17 15:23:58 +0530

    Extracted common css props into a new component

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,15 +1,16 @@
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
+import Card from "./Card";
 
 function Expenses(props) {
     const item = props.item;
     return (
-        <div className="expenses">
+        <Card className='expenses'>
             <ExpenseItem name={item[0].name} price={item[0].price} date={item[0].date}/>
             <ExpenseItem name={item[1].name} price={item[1].price} date={item[1].date}/>
             <ExpenseItem name={item[2].name} price={item[2].price} date={item[2].date}/>
             <ExpenseItem name={item[3].name} price={item[3].price} date={item[3].date}/>
-        </div>
+        </Card>
     );
 }
 
