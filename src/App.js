9b5b65ec4cb0594b4447eb12aac9da4c36c645de
Sonali <136353154+itsssona@users.noreply.github.com>
2023-06-17 15:59:25 +0530

    Just playing around with how JSX code gets coverted to React composition of elements

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Expenses from './components/Expenses';
 
 function App() {
@@ -8,11 +9,17 @@ function App() {
     { id: 4, name: 'Table mat', price: 97.56, date: new Date(2023, 5, 12)},
   ];
 
-  return (
-    <div>
-      <Expenses item={expenses}/>
-    </div>
-  );
+  return React.createElement(
+    'div', 
+    {}, 
+    React.createElement(Expenses, { item: expenses })
+    );
+
+//   return (
+//     <div>
+//       <Expenses item={expenses}/>
+//     </div>
+//   );
 }
 
 export default App;
